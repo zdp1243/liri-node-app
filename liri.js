31f448e89a6b75c5c7d82c1b4d97493dab7143b6
Jacinda Power <jjpower.writes@gmail.com>
2018-04-04 13:40:02 -0700

    added npm, package.json, dotenv, lodash, require, spotify, and twitter npm

--- liri.js
+++ liri.js
@@ -3,6 +3,12 @@
 require("dotenv").config();
 
 ```
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
 
 var Twitter = require('twitter');
  
@@ -28,16 +34,24 @@ client.stream(path, params, callback);
 
   var params = {screen_name: 'nodejs'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
+  if (!error && response.statusCode === 200) {
     console.log(tweets);
+  //  console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
   }
 });
 
-//Retrieves last 20 Tweets (copied from npm Twitter)
-var params = {
-    screen_name: 'yourTwitterUsername'
-} && {
-    count: 20
-};
+
+// Then run a request to the OMDB API with the movie specified
+request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", 
+function (error, response, body) {
+  
+  // If the request is successful (i.e. if the response status code is 200)
+  if (!error && response.statusCode === 200) {
+
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    
+  }
+});
 
 
